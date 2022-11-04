b7ce61b5b50853bf21afe071865cbf24990578a5
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-11-04 18:23:24 -0400

    Added comments for API key

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //modules
 const express = require('express');
-const https = require('node:https'); //native to node
+const https = require('node:https'); //native to node - could use fetch, axios etc
 
 const app = express();
 const port = 3000;
 
 const lat = 43.4819027;
 const lon = -79.8427059;
-const apiKey = 'aaf8ff4104d2f272e069a0be058e3533';
+const apiKey = 'aaf8ff4104d2f272e069a0be058e3533'; //normally would store this elsewhere
 
 const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
