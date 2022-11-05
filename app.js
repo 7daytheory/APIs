b2fe8fa76d2c06f3162e41767b9cf0de458d9e05
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-11-04 22:14:35 -0400

    Use write to add multiple items to page - use city instead of lat and lon

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 //modules
 const express = require('express');
-const https = require('node:https'); //native to node - could use fetch, axios etc
+const https = require('node:https'); //native to node - could use axios or others etc
 
 const app = express();
 const port = 3000;
-
-const lat = 43.4819027;
-const lon = -79.8427059;
 const apiKey = 'aaf8ff4104d2f272e069a0be058e3533'; //normally would store this elsewhere
 
-const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
 app.get("/", (req, res) => {
+
+  const query = "Toronto";
+  const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=metric`;
+
   https.get(weatherURL, (response) => {
     console.log(response.statusCode)
 
     response.on("data", (data) => {
       const weatherData = JSON.parse(data);
       console.log(weatherData);
-      const weatherTemp = weatherData.main.temp + "(Seriously?)";
-      const weatherDesc = weatherData.weather[0].description;
+      const weatherTemp = weatherData.main.temp; //temp is showing up as 255+ lol
+      const weatherDesc = weatherData.weather[0].description; //description object inside a single array
+      const weatherIcon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + weatherIcon + ".png";
 
-      console.log("Temperature " + weatherTemp + " with " + weatherDesc);
+      res.write("<h1>The temperate is " + weatherTemp + " degrees</h1>");
+      res.write("<h3>Description: " + weatherDesc + "</h3>");
+      res.write("<img src=" + imageURL + " alt='icon' />");
+      res.send();
     })
   })
-  res.send("View console log for weather data(for now)");
 })
 
 app.listen(port, () => {
