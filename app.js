3c679604c3f6fb778d082fcffe751abb7689f2e0
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-11-04 22:29:45 -0400

    Removed commented out code

--- app.js
+++ app.js
@@ -40,25 +40,3 @@ app.post("/", (req,res) => {
 app.listen(port, () => {
   console.log("Listening on " + port);
 })
-
-// const query = "Toronto";
-// const units = "metric";
-// const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${units}`;
-//
-// https.get(weatherURL, (response) => {
-//   console.log(response.statusCode)
-//
-//   response.on("data", (data) => {
-//     const weatherData = JSON.parse(data);
-//     console.log(weatherData);
-//     const weatherTemp = weatherData.main.temp; //temp is showing up as 255+ lol
-//     const weatherDesc = weatherData.weather[0].description; //description object inside a single array
-//     const weatherIcon = weatherData.weather[0].icon;
-//     const imageURL = "http://openweathermap.org/img/wn/" + weatherIcon + ".png";
-//
-//     res.write("<h1>The temperate is " + weatherTemp + " degrees</h1>");
-//     res.write("<h3>Description: " + weatherDesc + "</h3>");
-//     res.write("<img src=" + imageURL + " alt='icon' />");
-//     res.send();
-//   })
-// })
